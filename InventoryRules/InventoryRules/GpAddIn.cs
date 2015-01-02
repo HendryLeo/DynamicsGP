fbf950ef51798ad75fe5971c25b7d8f6104525af
HendryLeo <ghost.orchid2001@gmail.com>
2015-01-02 13:36:35 +0700

    InventoryRules - Various GP Rules concerning Inventory that can not be done via GP settings

---
+++ InventoryRules/InventoryRules/GpAddIn.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Dexterity.Bridge;
+using Microsoft.Dexterity.Applications;
+using Microsoft.Dexterity.Applications.VvfIdInHouseCustomizationDictionary;
+using Microsoft.Dexterity.Applications.VvfDictionary;
+
+namespace InventoryRules
+{
+    public class GPAddIn : IDexterityAddIn
+    {
+        // IDexterityAddIn interface
+        
+        public void Initialize()
+        {
+            
+        }
+    }
+}
---
+++ InventoryRules/README.txt
@@ -0,0 +1,5 @@
+This Project is to enforce business rules that can not be achieved using GP settings alone
+1. Shipment (Receiving Trx Entry) must use today Date
+
+
+As usual, I have several alternate form
