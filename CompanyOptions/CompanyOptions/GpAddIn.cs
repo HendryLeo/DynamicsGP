3cf8b73f1e2b6f03e8d3ab9a4ad0dba992c3eb48
HendryLeo <ghost.orchid2001@gmail.com>
2015-01-19 15:30:07 +0700

    CompanyOptions - Release 1.0.0

--- CompanyOptions/CompanyOptions/GpAddIn.cs
+++ CompanyOptions/CompanyOptions/GpAddIn.cs
@@ -16,15 +16,24 @@ namespace CompanyOptions
         static SwitchCompanyForm.SwitchCompanyWindow SwitchCompanyWindow = SwitchCompanyForm.SwitchCompany;
 
         static SyCurrentActivityTable ActivityTable = Microsoft.Dexterity.Applications.Dynamics.Tables.SyCurrentActivity;
+        
+        //System.Timers.Timer aTimer = new System.Timers.Timer();//using timer cause memory corruption, can not figure out how
+        //string userActivity = "";
 
         public void Initialize()
         {
             // Register Event to trigger when Switch Company form opens
-           SwitchCompanyForm.OpenAfterOriginal += new EventHandler(SwitchCompanyFormPre);
-
+            SwitchCompanyWindow.OpenAfterOriginal += new System.EventHandler(SwitchCompanyWindow_OpenAfterOriginal);
+            //SwitchCompanyWindow.CloseBeforeOriginal += new System.ComponentModel.CancelEventHandler(SwitchCompanyWindow_CloseBeforeOriginal);
         }
 
-        void SwitchCompanyFormPre(object sender, EventArgs e)
+        //void SwitchCompanyWindow_CloseBeforeOriginal(object sender, System.ComponentModel.CancelEventArgs e)
+        //{
+        //    aTimer.Stop();
+        //    //aTimer = null;
+        //}
+
+        void SwitchCompanyWindow_OpenAfterOriginal(object sender, EventArgs e)
         {
             List<string> users;
             TableError err;
@@ -41,7 +50,7 @@ namespace CompanyOptions
 
             users = new List<string>();
             users.Clear();
-            
+
             err = ActivityTable.GetFirst();
             while (err == TableError.NoError)
             {
@@ -49,13 +58,39 @@ namespace CompanyOptions
                 err = ActivityTable.GetNext();
             }
             ActivityTable.Close();
+
             //create string scrolling procedure here to display all user activity, otherwise Status is limited to 62 char
-            foreach( string str in users.ToArray())
+            foreach (string str in users.ToArray())
             {
+                //userActivity += str + ",";
                 SwitchCompanyWindow.LocalStatus.Value += str + ",";
             }
             //remove trailing comma
-            //SwitchCompanyWindow.LocalStatus.Value = SwitchCompanyWindow.LocalStatus.Value;
+            //userActivity = userActivity.Substring(0, userActivity.Length - 1);
+            //if (userActivity.Length > 1)
+            //{
+            //    aTimer.Elapsed += aTimer_Elapsed;
+            //    aTimer.Interval = 1000;
+            //    aTimer.Start();
+            //}
         }
+
+        //void aTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        //{
+        //    //Application.DoEvents();
+        //    try
+        //    {
+        //        if (SwitchCompanyWindow != null)
+        //        {
+        //            SwitchCompanyWindow.LocalStatus.Value = userActivity;
+        //            userActivity = userActivity.Substring(1) + userActivity.Substring(0, 1);
+        //        }
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        aTimer.Stop();
+                
+        //    }
+        //}
     }
 }
